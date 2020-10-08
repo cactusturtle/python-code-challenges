7669ef0deb84edf35262ad58201741315fbbe5c1
cactusturtle <66890398+cactusturtle@users.noreply.github.com>
2020-10-08 17:27:01 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,11 @@
+
+#var to store user input
+color_1 = input("Color: ")
+color_2 = input("Another Color :")
+pronoun_ = input("Noun: ")
+
+#input user data and print out madlib
+print("Roses are " + color_1)
+print("Violets are " + color_2)
+print("I love " + pronoun_)
+
